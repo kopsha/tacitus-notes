050840a3a406014a231060584f87e7532935c12d
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2021-10-16 13:30:56 +0300

    ready, including body details

--- tacitus.py
+++ tacitus.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+import re
 import subprocess
 
 
 def exec(command):
-    print(command)
+    """runs shell command and capture the output"""
+
     result = subprocess.run(command, shell=True, capture_output=True)
 
     if result.returncode:
@@ -12,18 +14,89 @@ def exec(command):
     return result.stdout.decode("utf-8").strip(), result.returncode
 
 
+def parse_history(raw_log):
+    """given raw git log, parses into commit subject and body"""
+
+    history = list()
+    remaining = raw_log
+    sep = True
+    while sep:
+        raw_entry, sep, remaining = remaining.partition("<<<")
+        subject, x_sep, body = raw_entry.partition(">>>")
+        if x_sep:
+            subject = subject.strip()
+            body = body.strip()
+            history.append((subject, body))
+        else:
+            assert not bool(subject.strip()), f"Cannot skip non-empty sequence: {subject.strip()}"
+
+    return history
+
+
+def generate_release_notes(history):
+    updates = list()
+    fixes = list()
+
+    is_fix = re.compile(r"(fix|fixes|fixed|hotfix)", re.IGNORECASE)
+    for subject, body in history:
+        notes = list()
+        subject = re.sub(r"^\*\s+", r"", subject)
+        notes.append(f"* {subject.capitalize()}")
+
+        if body:
+            for line in body.split("\n"):
+                line = line.strip()
+                if line.startswith("Co-authored-by:"):
+                    continue
+
+                extra = re.sub(r"^\*\s+", r"", line)
+                if extra:
+                    notes.append(f"  * {extra.capitalize()}")
+
+        full_note = "\n".join(notes)
+        if is_fix.match(subject):
+            fixes.append(full_note)
+        else:
+            updates.append(full_note)
+
+
+    release_notes = list()
+    release_notes.extend(("# Release title", "", "",))
+
+    if updates:
+        release_notes.extend(("## Features and improvements", "",))
+        release_notes.extend(updates)
+        release_notes.extend(("", ""))
+
+
+    if fixes:
+        release_notes.extend(("## Fixes", "",))
+        release_notes.extend(fixes)
+
+    text = "\n".join(release_notes)
+    return text
+
+
 def main():
     since, error_code = exec("git describe --tags --abbrev=0")
     if error_code:
         # if no tag was found, get first commit
-        since, error_code = exec("git log --format='%h' | head -1")
+        since, error_code = exec("git rev-list --max-parents=0 HEAD | head -1")
+    else:
+        # pick only major version
+        since = re.sub(r"^(v\d+\.\d+).*", r"\1", since)
 
-    git_log, error_code = exec(f"git log {since}..HEAD --notes")
+
+    git_log, error_code = exec(f"git --no-pager log {since}..HEAD --format='%s>>>%b<<<'")
     if error_code:
         print("Cannot read git history, reason:", git_log)
 
-    print("history repeats itself")
-    print(git_log)
+    history = parse_history(git_log)
+    markdown_text = generate_release_notes(history)
+
+    print("----")
+    print(markdown_text)
+    print("----")
 
 
 if __name__ == "__main__":
