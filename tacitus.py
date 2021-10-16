3f30b75e01db33448ffffb66c8a04654d32ca1ba
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2021-10-16 11:53:43 +0300

    more history

--- tacitus.py
+++ tacitus.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python3
-import filecmp
-import os
 import subprocess
 
 
-def exec(command, debug=False):
-    output = subprocess.check_output(command, shell=True)
+def exec(command):
+    print(command)
+    result = subprocess.run(command, shell=True, capture_output=True)
 
-    if debug:
-        print(output.decode("utf-8"))
+    if result.returncode:
+        return result.stderr.decode("utf-8").strip(), result.returncode
 
-    return output.decode("utf-8")
+    return result.stdout.decode("utf-8").strip(), result.returncode
 
 
 def main():
-    exec("git log $(git describe --tags --abbrev=0)..HEAD --notes", debug=True)
+    since, error_code = exec("git describe --tags --abbrev=0")
+    if error_code:
+        # if no tag was found, get first commit
+        since, error_code = exec("git log --format='%h' | head -1")
+
+    git_log, error_code = exec(f"git log {since}..HEAD --notes")
+    if error_code:
+        print("Cannot read git history, reason:", git_log)
+
+    print("history repeats itself")
+    print(git_log)
 
 
 if __name__ == "__main__":
