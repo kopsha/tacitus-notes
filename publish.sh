6bb4f91d6761ccdc3bc57f7dd82b3b664c304d17
Florin Ciurcanu <florin.ciurcanu@gmail.com>
2021-12-13 07:22:09 +0200

    checkpoint

--- publish.sh
+++ publish.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 set -e
 
-rm -rf dist/*
-python3 -m build
-twine upload -r testpypi dist/*
+# rm -rf dist/*
+# python3 -m build
+# twine upload -r testpypi dist/*
+
+main()
+{
+    printf "commands queue '$*'\n"
+    while [[ $1 ]]
+    do
+        command=$1
+        printf " $command\n"
+        shift
+    done
+
+}
+
+main "$@"
